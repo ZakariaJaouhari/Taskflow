e1f9dc1985b84073aeed6ada905f6024e25b05d9
ZakariaJaouhari <zkrjaouhari05@gmail.com>
2026-03-27 15:47:49 +0100

    TP3

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { AuthProvider } from './features/auth/AuthContext'; 
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AuthProvider } from "./features/auth/AuthContext";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render( 
-  <StrictMode> 
-    <AuthProvider> 
-      <App /> 
-    </AuthProvider> 
-  </StrictMode> 
-); 
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
